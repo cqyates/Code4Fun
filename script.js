09f42d2c239896f0fe711e6d4b9ddfeb88ab3091
Corey Yates <cqyates@gmail.com>
2025-12-22 20:26:26 -0500

    added btn clicks to change css

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 console.log("connected")
 
 var colorBtn = document.getElementById("color-changer");
-
+var titleEl = document.getElementById("title")
+console.log(titleEl)
 colorBtn.addEventListener("click", function(event){
-  document.getElementById("title").classList.replace("text-success", "text-danger")
+  document.getElementById("title").classList.replace("text-success", "text-danger");
+  var heartIcon = document.createElement("i")
+  heartIcon.setAttribute("class", "bi bi-suit-heart-fill" )
+  
+  titleEl.append(heartIcon)
 })
