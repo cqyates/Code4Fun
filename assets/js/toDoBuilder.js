9a15997a31fa99a669b06c1cdc74c48513dcc3cb
Corey Yates <cqyates@gmail.com>
2025-12-25 18:08:52 -0500

    changed array to array of objects with status

--- assets/js/toDoBuilder.js
+++ assets/js/toDoBuilder.js
@@ -1,52 +1,68 @@
-
 // struggling with the order of firing functions
 // the list crosses out but changes on submitting new item.
 // the delete function will work and then break when I change something unrelated, so that's weird
 // walking away for clarity
+//solution - must save as an array of objects with a status that starts as "to-do" and changes to "completed"
 
 const submitBtn = document.getElementById('add-task');
 const listContainer = document.querySelector('.list-container');
 const taskList = document.getElementById('task-list');
 
-const taskArray = JSON.parse(localStorage.getItem('task-array')) || [];
-var clearList = document.createElement('i');
-clearList.setAttribute('class', 'bi bi-trash');
-clearList.setAttribute('id', 'delete-all');
-clearList.setAttribute('type', 'button');
+// on submit button click, fire a function that creates and object with the task name and the status which defaults to "to-do"
+// save that object to the taskList array and save to localstorage.
 
-listContainer.append(clearList);
-clearList.addEventListener('click', function () {
-  localStorage.removeItem('task-array');
-  taskList.innerHTML = '';
+// var clearList = document.createElement('i');
+// clearList.setAttribute('class', 'bi bi-trash');
+// clearList.setAttribute('id', 'delete-all');
+// clearList.setAttribute('type', 'button');
 
-});
+// listContainer.append(clearList);
+// clearList.addEventListener('click', function () {
+//   localStorage.removeItem('task-array');
+//   taskList.innerHTML = '';
 
-const setLocalStorage = (arr) => {
-  localStorage.setItem('task-array', JSON.stringify(arr));
+// });
 
+// const setLocalStorage = (arr) => {
+//   localStorage.setItem('task-array', JSON.stringify(arr));
+
+// };
+
+const getOldTasks = () => {
+  return JSON.parse(localStorage.getItem('task-array')) || [];
 };
 const buildList = () => {
-  taskList.addEventListener("click", function(event){
-   
-    if (event.target && event.target.classList.contains('todo-item')) {
-      // Toggle the 'completed' class on the clicked item
-      event.target.classList.add('completed');
+  const listArray = getOldTasks();
+  listArray.forEach(({ name, status }) => {
+    var liEl = document.createElement('li');
+    liEl.textContent = name;
+    if (status === 'completed') {
+      liEl.style.textDecoration = 'line-through';
     }
-  })
-  taskList.innerHTML = '';
-  for (var i = 0; i < taskArray.length; i++) {
-    var listItem = document.createElement('li');
-    listItem.setAttribute("class", "todo-item")
-    listItem.textContent = taskArray[i];
-    taskList.append(listItem);
-  }
+    liEl.setAttribute("class", "todo-item")
+    taskList.append(liEl);
+  });
 };
+const changeStatus = () => {
+  const oldTaskArray = getOldTasks()
+  console.log(oldTaskArray);
+  // tasks:
+  // get Old Array.  
+  //Get value of li with this click
+  //use a filter method to find the correct line to change to completed
+  //save new list to localstorage.
+}
 submitBtn.addEventListener('click', function () {
   var taskName = this.previousElementSibling.value;
-  taskArray.push(taskName);
+  var task = {
+    name: taskName,
+    status: 'to-do',
+  };
   document.getElementById('task-name').value = '';
-  setLocalStorage(taskArray)
-  buildList()
+  const taskArray = getOldTasks();
+  taskArray.push(task);
+  localStorage.setItem('task-array', JSON.stringify(taskArray));
+  buildList();
+  //upcoming problem.  I don't think you can update a specific key/value pair in LS.
+  // would need to "write over" the list and if I go that direction I have to make sure that the old status persists through rewrites
 });
-
-
